dcf598c60df3e1a3c53156ca425036ddd26a734b
ParkHyeonChae <park19996@naver.com>
2020-08-11 15:23:39 +0900

    [FEATURE] NickName 중복검사 API 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+import requests, json
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny # 로그인 여부
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication # JWT인증
+from accounts.models import CustomUser as User
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((JSONWebTokenAuthentication,))
+def check_nickname(request):
+    """
+    Params--nickname
+    Returns
+        중복 닉네임 없을 시
+        message : success
+        status : 200
+        중복 닉네임 있을 시
+        message : nickname_exist
+        status : 409
+    """
+    try:
+        if User.objects.filter(nickname=request.GET['nickname']).exists():
+            return JsonResponse({
+                'result': 'fail',
+                'message': '이미 존재하는 닉네임입니다.'
+            }, status=409)
+        else:
+            return JsonResponse({
+                'result':'success',
+                'message': '사용 가능한 닉네임입니다.'
+            }, status=200)
+    except KeyError:
+        return JsonResponse({'result':'INVALID_KEY'})
